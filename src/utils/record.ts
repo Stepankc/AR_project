cabc48d350c1f097a4112fc71333d5e6cbe7cb41
dmitryqv <vlasenko_dima@icloud.com>
2022-11-16 16:15:44 +0500

    [FEATURE] Added record utils

---
+++ src/utils/record.ts
@@ -0,0 +1,105 @@
+export default class Record {
+  private stream: MediaStream | null;
+  constructor() {
+    this.stream = null;
+  }
+
+  /**
+   * Method for check video device
+   * @return boolean
+   */
+  async hasVideoInput() {
+    return (await navigator.mediaDevices.enumerateDevices()).some(
+      (device) => device.kind === "videoinput"
+    );
+  }
+
+  /**
+   * Method for start record
+   * @return Promise<Blob>
+   */
+  async start(HTMLVideoElement: HTMLVideoElement): Promise<Blob | void> {
+    /**
+     * Check video input
+     */
+    if (await this.hasVideoInput()) {
+      /**
+       * Start with mx & mh scale
+       */
+      this.stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+      });
+
+      /**
+       * @return video stream
+       */
+      return await this.startVideo(HTMLVideoElement);
+    }
+    /**
+     * @return Video Input Exception
+     */
+    throw new Error("[VIE] - free video input not found");
+  }
+
+  /**
+   * Method for stop record
+   * @return void
+   */
+  stop(): void {
+    if (this.stream) {
+      this.stream.getTracks().forEach((t) => t.stop());
+    }
+  }
+
+  /**
+   * Method for start video
+   * @return Promise<Blob>
+   */
+  async startVideo(HTMLVideoElement: HTMLVideoElement): Promise<Blob> {
+    return new Promise((resolve, reject) => {
+      /**
+       * Prefire check
+       */
+      if (this.stream) {
+        /**
+         * Initialize MediaRecorder
+         * Tempory dataavailable storage
+         */
+        const media = new MediaRecorder(this.stream);
+        const tmp: Blob[] = [];
+
+        /**
+         * Update tempory storage
+         */
+        media.addEventListener("dataavailable", ({ data }: BlobEventInit) =>
+          tmp.push(data)
+        );
+
+        /**
+         * Added handlers
+         */
+        new Promise((resolve, reject) => {
+          media.addEventListener("stop", resolve);
+          media.addEventListener("error", reject);
+        })
+          .then(() => {
+            /**
+             * Get payload after stop record
+             */
+            return resolve(new Blob(tmp, { type: "video/mp4" }));
+          })
+          /**
+           * @return Uknown Exception
+           */
+          .catch((e) => reject(e));
+
+        /**
+         * Start up
+         */
+        media.start();
+        HTMLVideoElement.srcObject = this.stream;
+      }
+    });
+  }
+}
