bd02b2eb21ff5d0c0fc435225f6620bc7e0a2caa
dmitryqv <vlasenko_dima@icloud.com>
2022-11-18 19:28:20 +0500

    [DEVELOPMENT] Added @redux/toolkit app hooks

---
+++ src/hooks/redux.useSelector.ts
@@ -0,0 +1,5 @@
+import { useSelector } from 'react-redux';
+import { TypedUseSelectorHook } from 'react-redux';
+// import { TypedRootState } from "../store/store";
+
+// export const useTypedSelector: TypedUseSelectorHook<TypedRootState> = useSelector;
