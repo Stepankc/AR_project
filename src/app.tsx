770316b80311aef79a424e300e474dd33b051214
dmitryqv <vlasenko_dima@icloud.com>
2022-11-22 00:32:56 +0500

    [REFACTORING] Refactoring app tsx

--- src/app.tsx
+++ src/app.tsx
@@ -5,13 +5,10 @@ import "./ui/reset.scss";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
-/**
- *  @deprecated
- *  Dont use
- *  import { RootView } from "./views/root.view";
- */
+/** @deprecated */
+import { RootView } from "./views/root.view";
 
-export const App: FunctionComponent = () => <>ar</>;
+const App: FunctionComponent = () => <>ar</>;
 
 export const AppCore: FunctionComponent = () => (
   <Provider store={store}>
