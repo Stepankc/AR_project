e23b784d2342151d6b52084f721e0ace34b5ea1a
i21s692 <i21s692@students.it-college.ru>
2022-11-21 13:47:11 +0500

    [FEATURE] Redux store init

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,19 @@ import React, { FunctionComponent } from "react";
 import "./ui/normalize.scss";
 import "./ui/reset.scss";
 
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
 /**
  *  @deprecated
  *  Dont use
  *  import { RootView } from "./views/root.view";
-*/
+ */
 
 export const App: FunctionComponent = () => <>ar</>;
+
+export const AppCore: FunctionComponent = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
