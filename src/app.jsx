ddccfd0c8dd558e81a797f78364c83a0e4a1ab9b
dmitryqv <vlasenko_dima@icloud.com>
2022-12-01 02:28:29 +0500

    [REFACTORING]

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
-import { ARCanvas } from "@artcom/react-three-arjs";
 import React from "react";
 import "./ui/reset.scss";
+
+import { ARCanvas } from "@artcom/react-three-arjs";
 import { Dino, Knight } from "./models/export";
 
 import { Provider } from "react-redux";
