f4b9dbac0de020c59269cc896d42afe1fe34c1b6
i21s692 <i21s692@students.it-college.ru>
2022-11-30 22:15:10 +0500

    test-AR

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,5 @@
+import { ARCanvas, ARMarker } from "@artcom/react-three-arjs"
+import {Canvas} from "@react-three/fiber";
 import React, { FunctionComponent } from "react";
 import "./ui/normalize.scss";
 import "./ui/reset.scss";
@@ -8,7 +10,26 @@ import { store } from "./store/store";
 /** @deprecated */
 import { RootView } from "./views/root.view";
 
-const App: FunctionComponent = () => <>ar</>;
+const App: FunctionComponent = () => (
+  <>
+  <ARCanvas
+    camera={ { position: [0, 0, 0] } }
+    onCreated={ ( gl:any ) => {
+      gl.setSize(window.innerWidth, window.innerHeight)
+    } }>
+    <ambientLight />
+    <pointLight position={ [10, 10, 0] }  />
+    <ARMarker
+      type={ "pattern" }
+      patternUrl={ "data/hiro.png" }>
+      <mesh>
+        <boxBufferGeometry args={ [1, 1, 1] } />
+        <meshStandardMaterial color={ "green" } />
+      </mesh>
+    </ARMarker>
+  </ARCanvas>,
+  </>
+);
 
 export const AppCore: FunctionComponent = () => (
   <Provider store={store}>
