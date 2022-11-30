4084834b12689c9a219c6076850afdb38294a74c
dmitryqv <vlasenko_dima@icloud.com>
2022-12-01 01:34:28 +0500

    [REFACTORING]

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AppCore } from "./app";
-// import "./webvitals";
 
 createRoot(document.querySelector("#app")).render(
   <StrictMode children={<AppCore />} />
