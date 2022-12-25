51da2d65f0e610b5c466d3db03e9b81464051537
i21s692 <i21s692@students.it-college.ru>
2022-12-25 20:45:07 +0500

    add_factoryEvent

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./ui/reset.scss";
 
 import { ARCanvas } from "@artcom/react-three-arjs";
@@ -7,8 +7,28 @@ import { Dino, Knight } from "@models/export";
 import { Provider } from "react-redux";
 import { store } from "@store/store";
 import { Perf } from "r3f-perf";
+import { events } from "@react-three/fiber"
+import { Html, useProgress } from '@react-three/drei'
+
+const eventManagerFactory = state => ({
+  ...events(state),
+
+  compute(event, state) {
+    state.pointer.set(
+      (event.clientX / state.size.width) * 2 - 1,
+      -(event.clientY / state.size.height) * 2 + 1,
+    )
+    state.raycaster.setFromCamera(state.pointer, state.camera)
+  },
+})
+
+function Loader() {
+  const { progress } = useProgress()
+  return <Html center>{progress} % loaded</Html>
+}
 
 const App = () => (
+
   <ARCanvas
     gl={{
       antialias: false,
@@ -20,12 +40,16 @@ const App = () => (
     onCreated={({ gl }) => {
       gl.setSize(window.innerWidth, window.innerHeight);
     }}
+    events={eventManagerFactory}
+    onPointerMissed={(e) => console.log(e, 'missed')}
   >
-    <Perf position="top-left"/>
-    <directionalLight castShadow position={[1,2,3]} />
+    <Perf position="top-left" />
+    <directionalLight castShadow position={[1, 2, 3]} />
     <ambientLight intensity={1.5} />
-    <Dino />
-    <Knight />
+    <Suspense fallback={<Loader />}>
+      <Dino />
+      <Knight />
+    </Suspense>
   </ARCanvas>
 );
 
